299cbaf3af670be409572f1e629b443febb9812a
Vince Panuccio <vince.panuccio@pacificmags.com.au>
2017-07-11 11:39:09 +1000

    Configure vim to install using package management

--- setup-env.ps1
+++ setup-env.ps1
@@ -1,12 +1,11 @@
 ﻿#Run this script the first time you log on to a machine you've never logged on to before.
 cls
 
-# The location of utilities
 $rootPath =         'E:\CloudStation'
-# The location of programing languages
 $rootDevLangPath  = 'D:\lang'
-$rootDevToolsPath = 'E:\CloudStation\tools'
-$scriptsPath      = 'D:\gitrepos\scripts'
+$rootDevToolsPath = "$rootPath\tools"
+$repoPath         = 'D:\gitrepos'
+$scriptsPath      = "$repoPath\scripts"
 
 $jdkVersion       = 'jre1.8.0_45'
 $antVersion       = '1.9.4'
@@ -36,19 +35,44 @@ $currentDir = Split-Path $MyInvocation.MyCommand.Definition
 Write-Host "Current directory is $currentDir"
 
 
-Write-Host 'Installing Chocolatey...' -ForegroundColor Green
+Write-Host 'Installing Chocolatey & packages...' -ForegroundColor Green
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 
-# Install Packages
-choco install python2
-choco install git
-choco install nodejs
+choco install python2 --limit-output
+choco install git --limit-output
+choco install nodejs --limit-output
 
 
 Write-Host 'Installing npm modules...' -ForegroundColor Green
 npm install -g firstaidgit
+# npm packages for vim syntastic javascript checker
+npm install -g eslint
+npm install -g eslint-plugin-react
+npm install -g babel-eslint
+npm install -g eslint-config-defaults
 Write-Host
 
+Write-Host 'Installing and configuring Vim...' -ForegroundColor Green
+choco install vim --limit-output
+
+# Install vim-plug
+md ~\vimfiles\autoload
+$uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+(New-Object Net.WebClient).DownloadFile(
+  $uri,
+  $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
+    "~\vimfiles\autoload\plug.vim"
+  )
+)
+# Get my vimrc from GitHub and symlink it to where Vim looks for it
+cd $repoPath
+git clone https://github.com/vincpa/vimrc
+cd ~\
+cmd /c mklink _vimrc  "$repoPath\vimrc\_vimrc"
+cmd /c mklink _gvimrc "$repoPath\vimrc\_gvimrc"
+c:
+cd 'Program Files\Vim\Vim80'
+gvim +PlugInstall
 
 Write-Host "Installing fonts for vim and powerline..." -NoNewLine
 $FONTS = 0x14
@@ -64,6 +88,13 @@ c:
 cd \python27\scripts
 pip install powerline-status
 
+$uri = 'https://raw.githubusercontent.com/powerline/powerline/bindings/vim/plugin/master/powerline.vim'
+(New-Object Net.WebClient).DownloadFile(
+  $uri,
+  $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
+    "c:\Program Files\Vim\Vim80\plugin\powerline.vim"
+  )
+)
 
 # Create the PowerShell profile directory if it doesn't exist
 $powershellProfileDir = [System.IO.Directory]::GetParent($PROFILE).FullName
