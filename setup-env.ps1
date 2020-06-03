458049ec00b61f97679ab9cf7cff1fa49170c682
Vince Panuccio <panuccio.vince@gmail.com>
2020-06-03 14:48:09 +1000

    Update scripts

--- setup-env.ps1
+++ setup-env.ps1
@@ -1,3 +1,5 @@
+$currentDir = Split-Path $MyInvocation.MyCommand.Definition
+
 # Look in both Program Files directories to find a child path
 function Get-ProgramPath($path, $childPath) {
 
@@ -53,40 +55,18 @@ function setEnvVariable($name, $value, $checkPath = $true)
     Write-Host "Set name $name and value $value" -ForegroundColor Green
 }
 
-
-# Install and configure Vim
-$vimTmpInstallScript = [System.IO.Path]::GetTempFileName() + '.ps1'
-(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/vincpa/vimrc/master/install-vim.ps1', $vimTmpInstallScript)
-Invoke-Expression "& `"$vimTmpInstallScript`" $repoPath"
-
-# Create the PowerShell profile directory if it doesn't exist
-$powershellProfileDir = [System.IO.Directory]::GetParent($PROFILE).FullName
-
-if (-not (Test-Path $powershellProfileDir)) {
-    mkdir $powershellProfileDir
-    Write-Host "Created PowerShell profile directory $powershellProfileDir" -ForegroundColor Green
-}
-
-
 # Modules
-Install-Module -Name Pscx -Force
-Install-Module -Name z -Force
+Install-Module -Name Pscx -Force -AllowClobber
+Install-Module -Name z -Force -AllowClobber
 Install-Module -Name Git-PsRadar -Force
 Install-Module -Name posh-git -Force
 
-
 # Symlink config files which are meant to be in the home directory to the repo
-New-HardLink "$env:USERPROFILE\.gitignore" "$currentDir\home-config\.gitignore"
-New-HardLink "$env:USERPROFILE\.gitconfig" "$currentDir\home-config\.gitconfig"
 
+New-Item -ItemType HardLink -Path "$env:USERPROFILE\.gitignore" -Target "$currentDir\config\.gitignore"
+New-Item -ItemType HardLink -Path "$env:USERPROFILE\.gitconfig" -Target "$currentDir\config\.gitconfig"
+New-Item -ItemType HardLink -Path "$env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Target "$currentDir\Microsoft.PowerShell_profile.ps1"
 
-setEnvVariable "PATH" "d:\gitrepos\psscripts"
-setEnvVariable "PATH" '%TOOLS%\UnixUtils'
-setEnvVariable "PATH" '%USERPROFILE%\AppData\Roaming\Python\Scripts'
-
-
-Write-Host
-Write-Host 'PowerShell profile installed. Restart PowerShell for settings to take effect.' -ForegroundColor Yellow
-Write-Host
-Write-Host "Root Path = $rootPath" -ForegroundColor Green
-Write-Host
+# setEnvVariable "PATH" "d:\gitrepos\psscripts"
+# setEnvVariable "PATH" '%TOOLS%\UnixUtils'
+# setEnvVariable "PATH" '%USERPROFILE%\AppData\Roaming\Python\Scripts'
--- setup-step1.ps1
+++ setup-step1.ps1
@@ -7,12 +7,10 @@ Write-Host "Current directory is $currentDir"
 
 
 Write-Host 'Installing Chocolatey & packages...' -ForegroundColor Green
-iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
-choco install python2 --limit-output -y
 choco install git --limit-output -y
 choco install nodejs --limit-output -y
-choco install ruby --limit-output -y
 choco install sysinternals --limit-output -y
 choco install winmerge --limit-output -y
 choco install rdcman --limit-output -y
@@ -22,19 +20,17 @@ choco install everything --limit-output -y
 choco install kdiff3 --limit-output -y
 choco install linqpad --limit-output -y
 choco install winmerge --limit-output -y
-choco install mc --limit-output -y
 choco install conemu --limit-output -y
 choco install 7zip.install --limit-output -y
-choco install choco install f.lux --limit-output -y
 choco install greenshot --limit-output -y
 choco install sumatrapdf --limit-output -y
 choco install paint.net --limit-output -y
 choco install filezilla --limit-output -y
+choco install unxutils --limit-output -y
 
 Write-Host 'Installing Scoop & packages...' -ForegroundColor Green
-iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
-scoop install perfview
 scoop install dnspy
 
 pause
